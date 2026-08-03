045389cad8238902dfd3610cef70c1a94faf24a2
Antoine <briocheguenot@gmail.com>
2026-08-03 10:44:52 +0200

    remove useless legacy functions and handled errors in some

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,4 +1,3 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 use bcrypt::{hash, verify, DEFAULT_COST};
 use magic_crypt::{new_magic_crypt, MagicCrypt256, MagicCryptTrait};
 use serde::{Deserialize, Serialize};
@@ -9,8 +8,6 @@ use std::sync::Mutex;
 
 #[derive(Serialize, Deserialize)]
 pub struct PasswordManager {
-    #[serde(skip)]
-    state: State,
     master_password: String,
     #[serde(skip)]
     encryption_key: Option<String>,
@@ -23,11 +20,54 @@ pub struct Password {
     password: String,
 }
 
-#[derive(Debug, PartialEq, Default)]
-enum State {
-    #[default]
-    Locked,
-    Unlocked,
+impl PasswordManager {
+    fn new(master_password: String) -> Self {
+        PasswordManager {
+            master_password: hash(master_password, DEFAULT_COST)
+                .expect("Error hashing the master password."),
+            password: HashMap::new(),
+            encryption_key: None,
+        }
+    }
+
+    fn load(path: String) -> Result<Self, &'static str> {
+        let new_path = format!("{}/passwords.json", path);
+        if let Ok(json_data) = fs::read_to_string(new_path) {
+            // File exists
+            if let Ok(mut manager) = serde_json::from_str::<PasswordManager>(&json_data) {
+                // We managed to read it properly
+                manager.encryption_key = None;
+                // no need for the master pass and the passwords because serde did it for us by serialize it from the json file.
+                return Ok(manager);
+            } else {
+                return Err("Error reading from json file, serde?");
+            }
+        }
+        Err("Error loading the password.json file.")
+    }
+
+    fn open_manager(&mut self, master_pass: String) -> Result<(), String> {
+        if verify(&master_pass, &self.master_password)
+            .map_err(|err| format!("Couldn't hash the password : {err}"))?
+        {
+            self.encryption_key = Some(master_pass);
+            Ok(())
+        } else {
+            Err("Wrong password".to_string())
+        }
+    }
+
+    fn save_config_file(&self, path: String) -> Result<(), String> {
+        let new_config_path = format!("{}/passwords.json", path);
+
+        let json_data = serde_json::to_string_pretty(self)
+            .map_err(|e| format!("Error serializing to json: {}", e))?;
+
+        std::fs::write(new_config_path, json_data)
+            .map_err(|e| format!("Error saving the file to disk: {}", e))?;
+
+        Ok(())
+    }
 }
 
 #[tauri::command]
@@ -207,143 +247,7 @@ fn modify_entry(
     }
 }
 
-impl PasswordManager {
-    pub fn new(master_password: String) -> Self {
-        PasswordManager {
-            state: State::Locked,
-            master_password: hash(master_password, DEFAULT_COST)
-                .expect("Error hashing the master password."),
-            password: HashMap::new(),
-            encryption_key: None,
-        }
-    }
-
-    pub fn load(path: String) -> Result<Self, &'static str> {
-        let new_path = format!("{}/passwords.json", path);
-        if let Ok(json_data) = fs::read_to_string(new_path) {
-            // File exists
-            if let Ok(mut manager) = serde_json::from_str::<PasswordManager>(&json_data) {
-                // We managed to read it properly
-                manager.encryption_key = None;
-                // no need for the master pass and the passwords because serde did it for us by serialize it from the json file.
-                return Ok(manager);
-            } else {
-                return Err("Error reading from json file, serde?");
-            }
-        }
-        Err("Error loading the password.json file.")
-    }
-
-    pub fn open_manager(&mut self, master_pass: String) -> Result<(), &'static str> {
-        if verify(&master_pass, &self.master_password)
-            .expect("Error hashing password to verify it.")
-        {
-            self.unlock_manager(master_pass);
-            Ok(())
-        } else {
-            Err("Wrong password.")
-        }
-    }
-
-    fn unlock_manager(&mut self, master_pass: String) {
-        self.state = State::Unlocked;
-        self.encryption_key = Some(master_pass);
-    }
-
-    pub fn close_manager(&mut self, path: String) {
-        self.state = State::Locked;
-        self.encryption_key = None;
-        self.save_config_file(path).expect("Error saving the file.")
-    }
-
-    pub fn add_password(
-        &mut self,
-        url: String,
-        username: String,
-        password: String,
-    ) -> Result<(), String> {
-        if self.state == State::Locked {
-            Err("The manager is locked.".to_owned())
-        } else {
-            let key = self.encryption_key.as_ref().unwrap();
-            let mc = new_magic_crypt!(key, 256);
-
-            let new_password = Password {
-                username,
-                password: encrypt_password(password, mc),
-            };
-
-            self.password.insert(url, new_password);
-            Ok(())
-        }
-    }
-
-    pub fn modify_password(
-        &mut self,
-        url: String,
-        username: String,
-        new_password: String,
-    ) -> Result<(), &'static str> {
-        if self.state == State::Locked {
-            return Err("The manager is locked.");
-        }
-        if let Some(entry) = self.password.get_mut(&url) {
-            let key = self.encryption_key.as_ref().unwrap();
-            let mc = new_magic_crypt!(key, 256);
-
-            entry.username = username;
-            entry.password = encrypt_password(new_password, mc);
-
-            Ok(())
-        } else {
-            Err("Url not found.")
-        }
-    }
-
-    pub fn delete_password(&mut self, url: String) -> Result<(), &'static str> {
-        if self.state == State::Locked {
-            return Err("The manager is locked.");
-        }
-        if self.password.remove(&url).is_some() {
-            Ok(())
-        } else {
-            Err("Url not found.")
-        }
-    }
-
-    pub fn list_passwords(&self) -> Result<(), &'static str> {
-        if self.state == State::Locked {
-            return Err("The manager is locked.");
-        }
-        let key = self.encryption_key.as_ref().unwrap();
-        let mc = new_magic_crypt!(key, 256);
-
-        for (url, entry) in self.password.iter() {
-            let decrypted_password = decrypt_password(entry.password.clone(), mc.clone());
-
-            println!(
-                "URL : {} | username : {} | password : {}",
-                url, entry.username, decrypted_password
-            );
-        }
-
-        Ok(())
-    }
-
-    fn save_config_file(&self, path: String) -> Result<(), String> {
-        let new_config_path = format!("{}/passwords.json", path);
-
-        let json_data = serde_json::to_string_pretty(self)
-            .map_err(|e| format!("Error serializing to json: {}", e))?;
-
-        std::fs::write(new_config_path, json_data)
-            .map_err(|e| format!("Error saving the file to disk: {}", e))?;
-
-        Ok(())
-    }
-}
-
-pub fn get_full_file_path(relative_path: &str) -> Result<String, &'static str> {
+fn get_full_file_path(relative_path: &str) -> Result<String, &'static str> {
     if let Ok(home) = env::var("HOME") {
         Ok(format!("{}{}", home, relative_path))
     } else {
@@ -351,7 +255,7 @@ pub fn get_full_file_path(relative_path: &str) -> Result<String, &'static str> {
     }
 }
 
-pub fn create_folder(path: &str) -> Result<(), String> {
+fn create_folder(path: &str) -> Result<(), String> {
     if fs::create_dir_all(path).is_err() {
         return Err(format!("Error creating the {} folder", path));
     }
